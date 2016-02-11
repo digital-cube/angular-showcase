c9b7ec9b5392d1291a344967eb19867edf5d56c3
Savo <savo.garovic@gmail.com>
2016-02-11 16:07:59 +0100

    Add layout module, add bootstrap layout, etc...

--- www/app/components/users/users.service.js
+++ www/app/components/users/users.service.js
@@ -5,16 +5,24 @@
 export default function (module) {
     module.service('UsersService', function () {
         let usersListMock = {
-            1: {id: 1, firstName: 'Savo', lastName: 'Garovic', email: 'savo.garovic@gmail.com', age: 29},
-            2: {id: 2, firstName: 'Igor', lastName: 'Jeremic', email: 'igor@digitalcube.com', age: 39}
+            1: {id: 1, firstName: 'Savo', lastName: 'Garović', email: 'savo.garovic@gmail.com', age: 29},
+            2: {id: 2, firstName: 'Igor', lastName: 'Jeremić', email: 'igor@digitalcube.com', age: 39},
+            3: {id: 3, firstName: 'Aleksandar', lastName: 'Đinđić', email: 'djindjic@gmail.com', age: 18},
+            4: {id: 4, firstName: 'Dejan', lastName: 'Đorović', email: 'dejan@digitalcube.rs', age: 18},
+            5: {id: 5, firstName: 'Ivo', lastName: 'Kovačević', email: 'ivo@digitalcube.rs', age: 18},
+            6: {id: 6, firstName: 'Mladen', lastName: 'Milićević', email: 'mladen@digitalcube.rs', age: 18},
+            7: {id: 7, firstName: 'Nebojša', lastName: 'Dolaš', email: 'nebojsa@bemapps.com', age: 18},
+            8: {id: 8, firstName: 'Nemanja', lastName: 'Adamov', email: 'nemanja.d.adamov@gmail.com', age: 18},
+            9: {id: 9, firstName: 'Slobodan', lastName: 'Dolinić', email: 'slobodan@digitalcube.rs', age: 18},
+            10: {id: 10, firstName: 'Vladimir', lastName: 'Perić', email: 'pervlad@gmail.com', age: 18},
+            11: {id: 11, firstName: 'Vukašin', lastName: 'Vučević', email: 'vukasin@digitalcube.rs', age: 18},
+            12: {id: 12, firstName: 'Vladimir', lastName: 'Tarailo', email: 'vladimirtarailo@gmail.com', age: 18},
+            13: {id: 13, firstName: 'Mladen', lastName: 'Marinković', email: 'mladen.marinkovic.788@gmail.com', age: 18},
+            14: {id: 14, firstName: 'Jovan', lastName: 'Đelić', email: 'jdjelic@gmail.com', age: 18}
         };
 
         this.getAllUsers = function () {
             return usersListMock;
-        };
-
-        this.sayHello = function () {
-            console.log('Hello, I am User service');
         };
 
         this.addNewUser = function (user) {
